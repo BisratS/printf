abc46fd8a1faff8742e65a7b1c3f80e81d845c7f
rutiye <ruthmoges57@gmail.com>
2022-08-08 11:07:13 -0700

    attempt

---
+++ printChar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * printChar - print character.
+ * @list: va_list.
+ *
+ * Return 1
+ */
+
+int printChar(va_list list)
+{
+	int i;
+
+	aux = va_arg(list, int);
+
+	_putchar(i);
+
+	return (1);
+}
